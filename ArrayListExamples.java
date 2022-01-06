a9f93483c6d11fcc82ef78285c3a9cca85126460
erichaney <ehaney@outlook.com>
2022-01-06 10:56:53 -0700

    Update ArrayListExamples.java

--- ArrayListExamples.java
+++ ArrayListExamples.java
@@ -3,15 +3,25 @@ import java.util.ArrayList;
 class ArrayListExamples
 {
 
-    ArrayList<String> favoriteFoods;
-
-    void makeFoodList()
+    static void breakfastMenu()
     {
-        favoriteFoods.add("pancakes");
-        favoriteFoods.add("bacon");
-        favoriteFoods.add("tater tots");
+       ArrayList<String> menu = new ArrayList<String>();
+       
+       menu.add("Bacon");
+       menu.add("Toast");
+       menu.add("Pancakes");
+       
+       for (String m : menu)
+       {
+           System.out.println(m);
+       }
         
-        
+       menu.add(0, "Coffee");
+       menu.add(2, "OJ");
+       
+       for (String m : menu)
+       {
+           System.out.println(m);
+       }
     }
-    
 }
